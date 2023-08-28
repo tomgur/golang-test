3e75e1329e79bfd59abf41bd0badc7ff9361a1c8
Tom Gur <tom.gur@zoominfo.com>
2023-08-28 03:44:32 +0300

    feat: add the react-app

---
+++ random-quote-app/README.md
@@ -0,0 +1,23 @@
+# Demo React App
+
+This is a demo React app that communicates with the [Random Quote API go app]
+and uses 2 APIs 
+*random-quote*
+*bitcoin-price* 
+and displays the results in a table.
+
+The go app must be running for the React app to work.
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+You should use your local address (i.e. 192.168.0.12:3000), otherwise CORS will block the request to the API.
+
+The page will reload when you make changes.\
+You may also see any lint errors in the console.
+
+
---
+++ random-quote-app/src/App.js
@@ -0,0 +1,97 @@
+import React, {useEffect, useState} from 'react';
+import './App.css';
+
+function App() {
+  const [time, setTime] = useState(new Date());
+  const [quote, setQuote] = useState('');
+  const [author, setAuthor] = useState('');
+  const [category, setCategory] = useState('');
+  const [price, setBitcoinPrice] = useState(0.0);
+
+  function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  const fetchRandomQuote = async () => {
+    try {
+      const response = await fetch('http://192.168.10.143:8080/random-quote');
+      const data = await response.json();
+      console.info("quote: " + data.QUOTE)
+      console.info("author: " + data.AUTHOR)
+      console.info("category: " + data.CATEGORY)
+      var fixedCategory = capitalizeFirstLetter(data.CATEGORY)
+      setQuote(data.QUOTE);
+      setAuthor(data.AUTHOR);
+      setCategory(fixedCategory);
+    } catch (error) {
+      console.error('Error fetching quote:', error);
+    }
+  };
+
+  const fetchBitcoinPrice = async () => {
+    try {
+      const response = await fetch('http://192.168.10.143:8080/bitcoin-price');
+      const data = await response.json();
+      console.info("price: " + data)
+      setBitcoinPrice("$" + data);
+    } catch (error) {
+      console.error('Error fetching price:', error);
+    }
+  }
+
+  const handleQuoteChange = (event) => {
+    setQuote(event.target.value);
+    setAuthor(event.target.value);
+    setCategory(event.target.value);
+  };
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+const handlePriceChange = (event) => {
+    setBitcoinPrice(event.target.value);
+};
+
+  return (
+      <div className="App" style={{ backgroundColor: 'lightblue', minHeight: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <div className="centered">
+          <p>{time.toLocaleTimeString()}</p>
+          <textarea
+              placeholder="Random quote will appear here"
+              value={quote}
+              cols={30}
+              rows={10}
+              readOnly
+              onChange={handleQuoteChange}
+          /><br></br>
+          Author: <input
+              type="text"
+              placeholder="Author"
+              value={author||''}
+              onChange={handleQuoteChange}
+          /><br></br>
+          Category: <input
+              type="text"
+              placeholder="Category"
+              value={category||''}
+              onChange={handleQuoteChange}
+          /><br></br>
+          <button onClick={fetchRandomQuote}>Get Random Quote</button>
+          <p>Bitcoin Price</p>
+          <input
+              type="text"
+              placeholder="Bitcoin Price"
+              value={price||''}
+              onChange={handlePriceChange}
+          />
+          <button onClick={fetchBitcoinPrice}>Get Bitcoin Price</button>
+        </div>
+      </div>
+  );
+}
+
+export default App;
